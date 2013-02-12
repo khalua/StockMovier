c72e7b46933e6dcb89a6b6114ef03334cf75cfd0
Tony Contreras <tony.contreras@gmail.com>
2013-02-12 10:20:05 -0500

    nice finish

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -3,15 +3,15 @@ class MovieController < ApplicationController
   end
 
   def query
-    @search = params[:search].gsub(" ","+").gsub("'","")
-    @a = HTTParty.get("http://www.omdbapi.com/?t=#{@search}")
-    @b = JSON(@a)
-    @title = @b["Title"]
-    @year = @b["Year"].to_i
-    @rated = @b["Rated"]
-    @director = @b["Director"]
-    @plot = @b["Plot"].gsub("'","")
-    @poster = @b["Poster"]
+    search = params[:search].gsub(" ","+").gsub("'","")
+    a = HTTParty.get("http://www.omdbapi.com/?t=#{search}")
+    b = JSON(a)
+    @title = b["Title"]
+    @year = b["Year"].to_i
+    @rated = b["Rated"]
+    @director = b["Director"]
+    @plot = b["Plot"].gsub("'","")
+    @poster = b["Poster"]
 
     render 'results'
   end
